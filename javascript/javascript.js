6c2c95122363cfa4a62ce1a0836ce5c3bf265c8e
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-06-19 16:46:47 -0400

    ajout du js de la validation du checkbox

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,11 @@ let nombreDeLivre = document.querySelector(".nombre-de-livre p:last-child");
 let nombreDeLivreLus = document.querySelector(".nombre-de-livre-lus p:last-child");
 let nombreDeLivreNonLus = document.querySelector(".nombre-de-livre-non-lus p:last-child");
 
+let titreLivre = document.querySelector("#titre");
+let Auteur = document.querySelector("#auteur");
+let nombreDePage = document.querySelector("#page");
+let lus = document.querySelector("#checkbox")
+
 let boutonFerme = document.querySelector(".ferme");
 let ajouterLivre = document.querySelector(".ajout-livre button");
 
@@ -10,6 +15,14 @@ let livreLus = 0;
 let livreNonLus =0;
 
 let visible = false;
+
+function validate(){
+    if(lus.checked === true){
+        lus.value = true;
+    }else if(lus.checked === false){
+        lus.value = false;
+    }
+}
 
 ajouterLivre.addEventListener("click", ()=>{
 
